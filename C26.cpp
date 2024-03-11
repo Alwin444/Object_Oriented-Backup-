0e8fddbfa5a78bb2e948eda5da081bcb8e012410
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-03-11 17:20:22 +0530

    Create C26.cpp

---
+++ C26.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array[5] = {1,2,3,4,5},*ptr,i;
+    ptr = array;
+    cout<<"Base address of array: "<<array<<endl;
+    cout<<"Address stored by pointer: "<<ptr<<endl;
+    cout<<"First element in the array: "<<array[0]<<endl;
+    cout<<"Content in the pointer: "<<*ptr<<endl;
+    cout<<"--------------------Incrementing pointer---------------------"<<endl;
+    for(i=1;i<5;i++)
+     cout<<"Content in addres"<<(ptr+i)<<" is: "<<*(ptr+i)<<endl;
+     ptr = &array[4];
+    cout<<"--------------------Decrementing pointer---------------------"<<endl;
+    for(i=0;i<5;i++)
+    {
+     cout<<"Content in addres"<<(ptr)<<" is: "<<*(ptr)<<endl;
+     ptr--;
+    }
+    return 0;
+}
